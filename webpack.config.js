9ec6ee96e778073182a630814d9dbfbd49b14831
高林秀 <s.takabayashi@super-one.co.jp>
2021-07-09 13:06:36 +0900

    webpack５のビルドにてtsとhtmlの紐付け完了。あとはcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode:'development',
     entry: {
@@ -9,18 +11,36 @@ module.exports = {
         filename:'index.js'
     },
     module: {
-        rules: [{
-            test:/\.ts$/,
-            use:'ts-loader'
-        }]
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+            {
+                test:/\.css/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: "css-loader",
+                        options: {url:false}
+                    }
+                ]
+            }
+        ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
     resolve: {
         modules: [
             "node_modules",
         ],
         extensions: [
             '.ts',
-            '.js'
+            '.js',
+            '.css'
         ]
     }
 }
